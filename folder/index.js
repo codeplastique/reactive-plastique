b1de3f8f83465f4e274117ce3e7785e2d3a36168
codeplastique <rockets@bk.ru>
2019-09-06 16:49:15 +0300

    temp

--- folder/index.js
+++
@@ -1,7 +0,0 @@
-var fs = require('fs');
-let webpack = require("webpack");
-let path = require("path");
-const pug = require('pug');
-const vueCompiler = require('vue-template-compiler');
-const { JSDOM } = require('jsdom');
-
