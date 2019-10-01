c4c97947599933c01dacc7c270f6e8838a38e43d
codeplastique <rockets@bk.ru>
2019-10-01 16:07:05 +0300

    bug fix

--- index.js
+++ index.js
@@ -611,6 +611,10 @@ module.exports = {
     Plastique: Plastique,
     CompilePlugin: function(varToLibPath){
         varToLibPath = varToLibPath || {};
+        for(let lib in varToLibPath){
+            if(varToLibPath[lib] == null || varToLibPath[lib] == '')
+                varToLibPath[lib] = path.join(__dirname, './compileUtils', 'empty.ts')
+        }
         let path = require("path");
         let webpack = require("webpack");
         return new webpack.ProvidePlugin(Object.assign(varToLibPath ,{
