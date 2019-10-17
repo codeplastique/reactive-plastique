6f3be44d8415db1722ec9b7d509fe7606df038f7
codeplastique <rockets@bk.ru>
2019-10-17 15:54:47 +0300

    bug fix

--- EventManager.ts
+++ EventManager.ts
@@ -1,3 +1,5 @@
+import AppEvent from "./AppEvent";
+
  class EventManager {
     private static listeners: {[eventName: string]: Array<(event?: any) => Promise<any>>} = {} 
     private constructor(
@@ -10,23 +12,23 @@
         ///@ts-ignore
         EventManager.listeners[eventName].push(func);
     }
-    public fireEvent<T>(eventName: string, eventObject?: any): Promise<T[]>{
+    public fireEvent<T>(eventName: AppEvent<any>, eventObject?: any): Promise<T[]>{
         eventName = eventName.toLowerCase();
-        if(EventManager.listeners[eventName] == null){
+        if(EventManager.listeners[eventName as string] == null){
             console.log('No listeners for event: '+ eventName);
             ///@ts-ignore
             return Promise.resolve();
         }
-        return Promise.all(EventManager.listeners[eventName].map(func => func(eventObject)));
+        return Promise.all(EventManager.listeners[eventName as string].map(func => func(eventObject)));
     }
-    public fireEventOnParents<T>(eventName: string, eventObject?: any): Promise<T>{
+    public fireEventOnParents<T>(eventName: AppEvent<any>, eventObject?: any): Promise<T>{
         if(this.component == null)
             throw new Error('fireEventOnParents works only with components!')
         eventName = eventName.toLowerCase();
         let parent = this.component.app$.parent;
         while(parent){
-            if(parent.app$.events[eventName])
-                return Promise.resolve(parent.app$.events[eventName][0](eventObject));
+            if(parent.app$.events[eventName as string])
+                return Promise.resolve(parent.app$.events[eventName as string][0](eventObject));
             parent = parent.app$.parent;
         }
         console.log('No parent listeners for event: '+ eventName);
