2ad234878d64cd00d978be07d23496f738333fe0
codeplastique <rockets@bk.ru>
2019-11-29 18:30:01 +0300

    rever vue compiler

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ function Plastique(options){
     var glob = require('glob');
     var fs = require('fs');
     const pug = require('pug');
-    const vueCompiler = require('vue-template-es2015-loader');
+    const vueCompiler = require('vue-template-compiler');
     const { JSDOM } = require('jsdom');
     const ts = require("typescript");
     const PropertiesReader = require('properties-reader');
